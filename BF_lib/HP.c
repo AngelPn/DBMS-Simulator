402659ab770999c13a643356e98ab7d401bd1568
AngelPn <aggi.pa@gmail.com>
2020-11-26 19:58:50 +0200

    fixed logical errors

--- BF_lib/HP.c
+++ BF_lib/HP.c
@@ -79,17 +79,18 @@ int HP_InsertEntry(HP_info header_info, Record record){
         return -1;
     }
     previous_block = current_block;
-    current_block = current_block + NEXT;
+    current_block = *(int*)(current_block + NEXT);
 
     while ( block_num < BF_GetBlockCounter(header_info.fileDesc)-1) { //while (current_block != NULL)
         block_num++;
         memcpy(&count, current_block + REC_NUM, sizeof(int));
-        int j;
-        for (j = 0; j < count; j++){
+
+        for (int j = 0; j < count; j++){
             Record current_rec = (current_block + j*RECORD_SIZE);
 
             void *record_key = get_key(record, header_info.attrName);
             void *current_key = get_key(current_rec, header_info.attrName);
+            printf("keys %d %d\n", *(int *)record_key, *(int *)current_key);
             
             if (memcmp(record_key, current_key, header_info.attrLength) == 0){
                 return -1;
@@ -112,7 +113,7 @@ int HP_InsertEntry(HP_info header_info, Record record){
             BF_PrintError("Error reading block");
             return -1;
         }
-
+        //block-> void *, &block->void **
         memcpy(previous_block + NEXT, &block, sizeof(int));/*previous block points to this block*/
 
         count = 1;
@@ -133,32 +134,33 @@ int HP_InsertEntry(HP_info header_info, Record record){
 }
 
 int HP_DeleteEntry(HP_info header_info, void *value){
-    void *current_block; 
     int block_num=0;
+
+    void *current_block;
     if (BF_ReadBlock(header_info.fileDesc, 0,  &current_block) < 0){
         BF_PrintError("Error reading block");
         return -1;
     }
     current_block = *(int*)(current_block + NEXT);
 
-    while (current_block != NULL) {
-        void *block;
+    while ( block_num < BF_GetBlockCounter(header_info.fileDesc)-1) { //while (current_block != NULL)
         block_num++;
 
         int count;
-        memcpy(&count, block + REC_NUM, sizeof(int));
+        memcpy(&count, current_block + REC_NUM, sizeof(int));
    
         for (int j = 0; j < count; j++){
 
-            Record current_rec = (block + j*RECORD_SIZE);
+            Record current_rec = (current_block + j*RECORD_SIZE);
             void *current_key = get_key(current_rec, header_info.attrName);
 
             if (memcmp(value, current_key, header_info.attrLength) == 0){
 
-                Record last_rec = (block + count*RECORD_SIZE);
+                Record last_rec = (current_block + count*RECORD_SIZE);
+
                 count--;
-                memcpy(block + REC_NUM, &count, sizeof(int));
-                memcpy(last_rec, current_rec, RECORD_SIZE);
+                memcpy(current_block + REC_NUM, &count, sizeof(int));
+                memcpy(current_block + j*RECORD_SIZE, last_rec, RECORD_SIZE);
 
                 if (BF_WriteBlock(header_info.fileDesc, block_num) < 0)
                     return -1;
--- BF_lib/main.c
+++ BF_lib/main.c
@@ -14,27 +14,27 @@ int main(void){
     printf("%d %c %s %d\n", info->fileDesc, info->attrType, info->attrName, info->attrLength);
 
     Record rec = create_record(3, "Nikos", "rgregreg", "kkkkkk");
+    //Record rec = create_record(3, "Nikos", "rgregreg", "kkkkkk");
    // print_record(rec);
 
     HP_InsertEntry(*info, rec);
+    
+    if (HP_InsertEntry(*info, rec)==-1) printf("-1\n");
+    // HP_InsertEntry(*info, rec);
+    // HP_InsertEntry(*info, rec);
+    // HP_InsertEntry(*info, rec);
+    // HP_InsertEntry(*info, rec);
+    // HP_InsertEntry(*info, rec);
+    // HP_InsertEntry(*info, rec);
     int key = 3;
     HP_GetAllEntries(*info, &key);
-    // HP_InsertEntry(*info, rec);
-    // HP_InsertEntry(*info, rec);
-    // HP_InsertEntry(*info, rec);
-    // HP_InsertEntry(*info, rec);
-    // HP_InsertEntry(*info, rec);
-    // HP_InsertEntry(*info, rec);
-    // HP_InsertEntry(*info, rec);
-
-    // int count = 4;
-    // HP_DeleteEntry(*info, &count);
+    HP_DeleteEntry(*info, &key);
     // printf("delete\n");
     // HP_InsertEntry(*info, rec);
 
-    // if (HP_CloseFile(info) < 0)
-    //     printf("not\n");
-    // else printf("success\n");
+    if (HP_CloseFile(info) < 0)
+        printf("not\n");
+    else printf("success\n");
 
     return 0;
 }
