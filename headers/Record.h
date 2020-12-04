9588148e501a07fcdc2631721c543b1d911e51e2
AngelPn <aggi.pa@gmail.com>
2020-12-04 16:35:04 +0200

    comments in record

--- headers/Record.h
+++ headers/Record.h
@@ -3,12 +3,19 @@
 
 #define RECORD_SIZE 94
 
-
+/*'struct record' is an incomplete struct*/
 typedef struct record *Record;
 
+/*Creates and returns a new record*/
 Record create_record(int id, char *name, char *surname, char *address);
+
+/*Finds the key of record determined by attrName and returns a pointer to it*/
 void *get_key(Record rec, char *attrName);
+
+/*Prints the fields of record*/
 void print_record(Record rec);
+
+/*Deallocates record*/
 void free_record(Record rec);
 
 #endif /*RECORD_H*/
