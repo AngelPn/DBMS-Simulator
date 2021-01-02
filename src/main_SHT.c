0e5f63b9ed3e484f08086acce13f503e60bebe9c
AngelPn <aggi.pa@gmail.com>
2021-01-02 19:09:46 +0200

    created main_SHT

---
+++ src/main_SHT.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/HT.h"
+#include "../include/SHT.h"
+#include "../include/Record.h"
+
+int main(void){
+
+
+    return 0;
+}
